fd1c8b19db3599750ca948f74afe936a1fc9796c
Muhammad Faraz <farazmaliknum37@gmail.com>
2026-03-07 22:38:59 +0500

    update code

---
+++ poly.py
@@ -0,0 +1,12 @@
+class Dog:
+    def speak(self):
+        return "Bark"
+
+class Cat:
+    def speak(self):
+        return "Meow"
+
+animals = [Dog(), Cat()]
+
+for animal in animals:
+    print(animal.speak())
